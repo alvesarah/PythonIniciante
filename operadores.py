410b389e86a4379f36824778fd958124249164b8
alvesarah <sarah.alves8@hotmail.com>
2022-06-18 15:40:00 -0300

    Operadores aritmétcos, lógicos e de comparação

---
+++ operadores.py
@@ -0,0 +1,79 @@
+#Operadores Aritméticos
+
+# Somar
+3 + 2
+
+# Subtrair
+3 - 2
+
+#Dividir
+10 / 4
+
+#Multiplicar
+10 * 4
+
+#Resto da Divisão
+10 % 3
+
+#Quadrado
+3 ** 2
+
+#Operadores de Comparação
+b = 5 - 3
+#b vai ser 2
+
+#b é igual a 2?
+b == 2 #True
+b == 3 #False
+
+#b é diferente de:
+b != 2 #False
+b != 3 #True 
+
+#b maior que:
+b > 3 #False
+b > 1 #True
+
+#b menor que:
+b < 3 #True
+b < 1 #False
+
+#b maior ou menor e igual que:
+b >= 2 #True
+b >= 1 #True
+b >= 3 #False
+
+b <= 2 #True
+b <= 3 #True
+b <= 1 #False
+
+#Operadores Lógicos
+
+# A e B (as duas precisam ser verdadeiras)
+A = True
+B = False
+
+A and B #False
+
+A and not B #True
+
+A = True
+B = True
+
+A and B #True
+
+# A ou B (um dos dois deve ser verdadeiro)
+A = True
+B = False
+
+A or B #True
+
+# not A (Sempre o inverso do que será colocado)
+A = False
+
+not A #True
+
+# not B (Sempre o inverso do que será colocado)
+B = True
+
+not B #False
