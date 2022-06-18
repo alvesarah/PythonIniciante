8e06a5fd5b78db2dba3dccf8bff64c0ea161adc5
alvesarah <sarah.alves8@hotmail.com>
2022-06-18 15:19:34 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,32 @@
+a = 3
+b = 4
+
+print(a+b)
+
+a = 4
+
+print(a+b)
+
+c = "3"
+
+# print(a+c) #vai dar erro
+
+d= "5"
+
+print(c+d) #somando strings
+
+# Para saber o tipo da variável
+alpha = 1
+delta = "banana"
+omega = 10.3
+beta = True
+
+print(type(alpha), type(delta), type(omega), type(beta))
+
+a, b, c = 1, 2, 3
+
+print(a+b+c)
+
+forma_correta_nome_variavel = "Essa é a sintaxe correta para a variável"
+
+forma_incorreta_nome_variavel = "Não quer dizer que não pode, porém não é a forma mais correta em termos de sintaxe"    
